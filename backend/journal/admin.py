4924362dac0d63c415d3f7271ab46a6286860ffd
OwenS3881 <owenski08@gmail.com>
2024-05-31 15:41:54 -0400

    Finished superuser config

--- backend/journal/admin.py
+++ backend/journal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+
 from .models import JournalEntry
 
 admin.site.site_header = "Owen's ePortfolio Admin"
@@ -7,13 +7,3 @@ admin.site.site_title = "Owen's ePortfolio Admin Area"
 admin.site.index_title = "Welcome to Owen's ePortfolio Admin Area"
 
 admin.site.register(JournalEntry)
-
-
-user = User.objects.create(
-    username = 'owens2',
-    email = 'owenski09@gmail.com',
-    is_superuser = True,
-    is_staff = True,
-)
-user.set_password('password')
-user.save()

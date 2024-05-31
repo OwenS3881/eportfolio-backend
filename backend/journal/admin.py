aafd9e679ea4e4176deec99acc3cf45d0d3c68bc
OwenS3881 <owenski08@gmail.com>
2024-05-31 15:31:26 -0400

    Adding superuser

--- backend/journal/admin.py
+++ backend/journal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
 from .models import JournalEntry
 
 admin.site.site_header = "Owen's ePortfolio Admin"
@@ -7,3 +7,12 @@ admin.site.site_title = "Owen's ePortfolio Admin Area"
 admin.site.index_title = "Welcome to Owen's ePortfolio Admin Area"
 
 admin.site.register(JournalEntry)
+
+if not User.objects.filter(is_superuser=True).first():
+    user = User.objects.create(
+        username = 'admin',
+        email = 'admin@mywebsite.com',
+        is_superuser = True,
+    )
+    user.set_password('some password')
+    user.save()

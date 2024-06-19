ecdc4edb6a4b72302fb3313439d9d13f78d52b8c
OwenS3881 <owenski08@gmail.com>
2024-06-19 18:22:00 -0400

    Cleaned up SkillsSection file

--- frontend/app/components/HomePage/SkillsSection.jsx
+++ frontend/app/components/HomePage/SkillsSection.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 import styles from "@/app/styles/SkillsSection.module.css";
 
-import Image from "next/image";
-
 import SkillCard from "../SkillCard";
 
-import SkillIcon from "../SkillIcon";
-import Tooltip from "../Tooltip";
+//Displays all of my technical skills
 
 const SkillsSection = () => {
   return (

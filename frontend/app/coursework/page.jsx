51fdfde9a1d3a15e574ceff9c1a88d57268e1dec
OwenS3881 <owenski08@gmail.com>
2024-06-20 16:11:03 -0400

    Removed frontend

--- frontend/app/coursework/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CourseworkPage = () => {
-  return <div>CourseworkPage</div>;
-};
-
-export default CourseworkPage;
--- frontend/app/research/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ResearchPage = () => {
-  return <div>ResearchPage</div>;
-};
-
-export default ResearchPage;

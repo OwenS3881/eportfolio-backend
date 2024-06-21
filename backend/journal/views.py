3b4be002f91c35facc6bef68b3b3416faf7fb5c9
OwenS3881 <owenski08@gmail.com>
2024-06-21 13:00:33 -0400

    Single journal entry api

--- backend/journal/views.py
+++ backend/journal/views.py
@@ -6,3 +6,7 @@ from .models import JournalEntry
 class JournalEntryRetrieve(generics.ListAPIView):
     serializer_class = JournalEntrySerializer
     queryset = JournalEntry.objects.all()
+
+class SingleJournalEntryRetrieve(generics.RetrieveAPIView):
+    serializer_class = JournalEntrySerializer
+    queryset = JournalEntry.objects.all().order_by('date').first()

ec6eb60df70de412daf416c6c966f3f82cba0b93
OwenS3881 <owenski08@gmail.com>
2024-06-21 13:17:04 -0400

    Maybe fixed it for real #3

--- backend/journal/urls.py
+++ backend/journal/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.JournalEntryRetrieve.as_view(), name="journal-entries"),
-    path("recent", views.SingleJournalEntryRetrieve.as_view(), name="recent-journal-entry"),
+    path("recent/", views.SingleJournalEntryRetrieve.as_view(), name="recent-journal-entry"),
 ]

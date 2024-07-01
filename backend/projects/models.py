e84306f7a6e7d610c6b98ecf81c8d8228f58bea4
OwenS3881 <owenski08@gmail.com>
2024-07-01 13:13:37 -0400

    Added dates for projects

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -4,6 +4,8 @@ from django.contrib.postgres.fields import ArrayField
 class Project(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateTimeField(blank=True, null=True)
     description = models.TextField()
     skills = models.CharField(max_length=1000)
     url = models.CharField(max_length=200)
